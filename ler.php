10460e58f746a0233b7d54926f99c57cd5139b2d
aluno <aluno@ifsc.edu.br>
2023-08-30 16:57:37 -0300

    ler

--- ler.php
+++ ler.php
@@ -1,7 +1,14 @@
 <?php
 /* includes/conexao.php */
-$dbname = 'imobiliaria';
+$dbname = 'chat2023';
 $user = 'root';
 $password = '';
 
 $conexao = new PDO('mysql:host=localhost; dbname='.$dbname,$user,$password);
+
+$sql = 'select * from msg order by id desc';
+$msgs = $conexao->query($sql);
+
+foreach($msgs as $m){
+    echo $m["nome"].': '.$m["msg"];
+}
