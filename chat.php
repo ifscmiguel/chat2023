ab23b5e12a2d3c414f88149772897ad5918d1689
aluno <miguel.zarth@ifsc.edu.br>
2023-09-27 13:59:06 -0300

    título do doc

--- chat.php
+++ chat.php
@@ -10,7 +10,7 @@ if (!trim($nome)) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Chat Técnico em Informática IFSC Garopaba</title>
     <link rel="stylesheet" href="style.css">
     <style>
        
